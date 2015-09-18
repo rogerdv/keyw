0863997bfdd5a680f13f316561b6b70284258c81
rogerdv <roger@ehtsc.tur.cu>
2015-09-18 16:00:36 -0500

    Migrated to new ui system

--- Assets/Scripts/UI/MsgList.cs
+++ Assets/Scripts/UI/MsgList.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MsgList : MonoBehaviour {
-	private static Vector2 scrollPosition;
-	private static string text;
 
 	// Use this for initialization
 	void Start () {
-		text = "";
+
 	}
 	
 	// Update is called once per frame
@@ -15,18 +14,12 @@ public class MsgList : MonoBehaviour {
 
 	}
 
-	void OnGUI() {
-		GUILayout.BeginArea (new Rect(5, Screen.height-100, Screen.width-500, Screen.height-90));
-		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (200), GUILayout.Height (50));
-		GUILayout.Label (text);
-		GUILayout.EndScrollView ();
-		GUILayout.EndArea();
-	}
-
-	public static void SetText(string Text){
-		text+= Text + "\n";;
-		// setting the "y" value of scrollPosition puts the scrollbar at the bottom
-		scrollPosition = new Vector2(scrollPosition.x, Mathf.Infinity);
+	public void SetText(string text){
+		foreach (Transform t in gameObject.GetComponentsInChildren<Transform>()) {
+			if (t.name == "Text") {
+				t.GetComponent<Text>().text = t.GetComponent<Text>().text+text + "\n";
+			}
+		}	
 	}
 }
 
