09e9b7efcaa29de43d85dce456d71455eb643523
rogerdv <roger@ehtsc.tur.cu>
2015-11-13 11:09:10 -0500

    ScriptableObject asset creators

---
+++ Assets/tools/CreateItemAsset.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class ItemAsset
+{
+	[MenuItem("Assets/Create/Item")]
+	public static void CreateAsset ()
+	{
+		CustomAssetUtility.CreateAsset<BaseItem>();
+	}
+}
+#endif
---
+++ Assets/tools/CreateSkillAsset.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class SkillAsset
+{
+	[MenuItem("Assets/Create/Base Skill")]
+	public static void CreateAsset ()
+	{
+		CustomAssetUtility.CreateAsset<BaseSkill>();
+	}
+}
+#endif
