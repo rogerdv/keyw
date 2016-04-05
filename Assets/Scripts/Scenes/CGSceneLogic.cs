475ffd77d5e40e151e9c5b09e92285a557fd5268
rogerdv <roger@ehtsc.tur.cu>
2016-04-05 11:29:55 -0500

    Small test of singleton GameInstance

--- Assets/Scripts/Scenes/CGSceneLogic.cs
+++ Assets/Scripts/Scenes/CGSceneLogic.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 
 public class CGSceneLogic : MonoBehaviour {
-
+	GameInstance gm;
 	// Use this for initialization
 	void Start () {
-	
+		gm = GameInstance.Instance;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//if defend quest is in final phase, spawn demons a the door
-		var player = GameObject.FindGameObjectWithTag ("Player");
+		var player = GameInstance.player;// gm.player; //GameObject.FindGameObjectWithTag ("Player");
+		if (player == null) {
+			GameInstance.InstancePlayer();
+		}
 		if (player.GetComponent<BaseCharacter> ().GetQuestStatus ("defend-getgear") == "combat") {
 			//send everybody to the gates
 
