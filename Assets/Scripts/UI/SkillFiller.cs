eadb97aa3064857a9aacbc4f49562e43ace65154
rogerdv <roger@ehtsc.tur.cu>
2016-04-13 11:23:59 -0500

    Changes

--- Assets/Scripts/UI/SkillFiller.cs
+++ Assets/Scripts/UI/SkillFiller.cs
@@ -1,30 +1,58 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Xml;
 using System.Collections;
 using System.Collections.Generic;
 
+class SkillDef {
+	public string name;
+	public string desc;
+}
 public class SkillFiller : MonoBehaviour {
 
 	public GameObject panel;		//list container
 	public GameObject elem;			//list element prefab
 	List<GameObject> skills;
-
-	string[] SkNames = {"Melee","Dodge","Evasion", "Swords", "Hammers", "Shields",
-		"Physical training", "Combat", "Daggers", "Wands", "Elemental Mastery: fire", 
-		"Elemental Mastery: lightning", "Elemental Mastery: earth", "Elemental Mastery: water",
-		"Elemental Mastery: air", "Subterfuge", "Lore"};
+	Dictionary<string,SkillDef> SkList;
 
 	// Use this for initialization
 	void Start () {
 		RectTransform rt;
+
+		TextAsset textAsset = (TextAsset) Resources.Load("skill-list");
+		if (!textAsset) Debug.Log("failed to load xml resource");
+		var doc = new XmlDocument();
+		
+		doc.LoadXml (textAsset.text);
+		SkList = new Dictionary<string, SkillDef> ();
+		XmlNodeList root = doc.SelectNodes ("skills/skill");
+		string id;		//for parsing
+		foreach (XmlNode node in root) {
+			id = node.Attributes.GetNamedItem("id").Value;
+			SkList[id] = new SkillDef();
+			SkList[id].name = node.Attributes.GetNamedItem("name").Value;
+			SkList[id].desc = node.Attributes.GetNamedItem("description").Value;
+		}//foreach node
 		skills = new List<GameObject>();
 		int i = 0;
-		foreach (string n in SkNames) {
+		foreach (KeyValuePair<string, SkillDef> pair in SkList) {
 			var skill = Instantiate (elem);
 
-			skill.GetComponentInChildren<Text> ().text = n;
+			skill.GetComponentInChildren<Text> ().text = pair.Value.name;
 			rt = skill.GetComponent<RectTransform> ();
 			rt.SetParent (panel.transform);
+
+			var evt = skill.GetComponent<EventTrigger>();
+			EventTrigger.Entry entry = new EventTrigger.Entry();
+			entry.eventID = EventTriggerType.PointerClick;
+
+			entry.callback = new EventTrigger.TriggerEvent();
+			UnityEngine.Events.UnityAction<BaseEventData> callback =
+				new UnityEngine.Events.UnityAction<BaseEventData>(elementClick);
+
+			entry.callback.AddListener(callback);
+			evt.triggers.Add(entry);
 			skills.Add(skill);
 		}
 
@@ -37,5 +65,9 @@ public class SkillFiller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void elementClick(BaseEventData eventData) {
+
 	}
 }
