766556edadde3b536ddf4537e46c17f79e6ece1f
rogerdv <roger@ehtsc.tur.cu>
2016-04-13 11:23:47 -0500

    Refactorized, still not working

--- Assets/Scripts/UI/InventoryWindow.cs
+++ Assets/Scripts/UI/InventoryWindow.cs
@@ -4,10 +4,9 @@ using UnityEngine.EventSystems;
 using System.Collections;
 
 public class InventoryWindow : MonoBehaviour {
-	public GameObject invGridPrefab;
+	public GameObject invGridPrefab;		//list element
 	public GameObject panelPrefab;		//info panel prefab
 	GameObject player;
-	GameObject ghost;		//clone of the widget being dragged
 	GameObject[] buttons; 
 
 	// Use this for initialization
@@ -16,7 +15,7 @@ public class InventoryWindow : MonoBehaviour {
 		var inv = gameObject;
 			
 		//create the list
-		var scrollArea = GameObject.Find("ScrollArea");		
+		var scrollArea = GameObject.Find("content");		
 			
 		var InvList = player.GetComponent<BaseCharacter> ().inventory;
 		buttons = new GameObject[InvList.Count];
@@ -28,13 +27,13 @@ public class InventoryWindow : MonoBehaviour {
 				
 			var rt = buttons[row].GetComponent<RectTransform>();
 			rt.SetParent(scrollArea.transform);
-			rt.anchoredPosition = new Vector2(-150,200-row*31);
+			//rt.anchoredPosition = new Vector2(-150,200-row*31);
 			//Debug.Log("Button not null");
 			foreach (Transform t in buttons[row].GetComponentsInChildren<Transform>()){
 					if (t.name ==  "icon") {							
 						var icon = t.GetComponent<Image>();
 						icon.sprite = Resources.Load<Sprite>(item.portrait);
-					} else if (t.name ==  "name") {
+					} else if (t.name ==  "Text") {
 						var text = t.GetComponent<Text>();
 						text.text = item.Name;
 					}
@@ -42,7 +41,7 @@ public class InventoryWindow : MonoBehaviour {
 			//setup events
 			/*var b = buttons[row].GetComponent<Button>();
 			b.onClick.AddListener(() => MouseClick(buttons[row]));*/
-			SetupEvents(buttons[row]);	
+			//SetupEvents(buttons[row]);	
 			row++;						
 			}
 	}
@@ -56,47 +55,30 @@ public class InventoryWindow : MonoBehaviour {
 
 	void SetupEvents(GameObject widget){
 
+
 		//Get the event trigger attached to the UI object
-		EventTrigger eventTrigger = widget.GetComponent<EventTrigger>();
+		EventTrigger eventTrigger = widget.AddComponent<EventTrigger>();
 		
 		//Create a new entry. This entry will describe the kind of event we're looking for
 		// and how to respond to it
-		EventTrigger.Entry drag = new EventTrigger.Entry();
+		EventTrigger.Entry ButtonDown = new EventTrigger.Entry();
 		
 		//This event will respond to a drop event
-		drag.eventID = EventTriggerType.Drag;
+		ButtonDown.eventID = EventTriggerType.PointerDown;
 		
 		//Create a new trigger to hold our callback methods
-		drag.callback = new EventTrigger.TriggerEvent();
+		ButtonDown.callback = new EventTrigger.TriggerEvent();
 		
 		//Create a new UnityAction, it contains our DropEventMethod delegate to respond to events
 		UnityEngine.Events.UnityAction<BaseEventData> callback =
-			new UnityEngine.Events.UnityAction<BaseEventData>(DragEventMethod);
+			new UnityEngine.Events.UnityAction<BaseEventData>(MouseClick);
 		
 		//Add our callback to the listeners
-		drag.callback.AddListener(callback);
+		ButtonDown.callback.AddListener(callback);
 		
 		//Add the EventTrigger entry to the event trigger component
-		eventTrigger.triggers.Add(drag);
+		eventTrigger.triggers.Add(ButtonDown);
 		
-		//Begin drag
-		EventTrigger.Entry begindrag = new EventTrigger.Entry();
-		
-		//This event will respond to a drop event
-		begindrag.eventID = EventTriggerType.BeginDrag;
-		
-		//Create a new trigger to hold our callback methods
-		begindrag.callback = new EventTrigger.TriggerEvent();
-		
-		//Create a new UnityAction, it contains our DropEventMethod delegate to respond to events
-		UnityEngine.Events.UnityAction<BaseEventData> bd_callback =
-			new UnityEngine.Events.UnityAction<BaseEventData>(BeginDragEventMethod);
-		
-		//Add our callback to the listeners
-		begindrag.callback.AddListener(bd_callback);
-		
-		//Add the EventTrigger entry to the event trigger component
-		eventTrigger.triggers.Add(begindrag);
 
 		//enter event
 		EventTrigger.Entry enter = new EventTrigger.Entry();
@@ -118,32 +100,19 @@ public class InventoryWindow : MonoBehaviour {
 		eventTrigger.triggers.Add(enter);
 	}
 
-	void MouseClick(GameObject obj){
-		Debug.Log("Button '" + obj.name + "' pressed!");
+	//handles mouse clicks (left and right)
+	void MouseClick(BaseEventData data){
+		Debug.Log("Button '" + data.selectedObject.name + "' pressed!");
 		//Debug.Log("Equip item '" + i.Name);
 	}
 
+	//mouse enters element
 	public void MouseEnter(UnityEngine.EventSystems.BaseEventData baseEvent){
 		Debug.Log(baseEvent.selectedObject.name + " triggered an enter event!");
 	}
 
 	//Create an event delegate that will be used for creating methods that respond to events
 	public delegate void EventDelegate(UnityEngine.EventSystems.BaseEventData baseEvent);
-
-	public void BeginDragEventMethod(UnityEngine.EventSystems.BaseEventData baseEvent) {
-		Debug.Log(baseEvent.selectedObject.name + " triggered a bgin drag event!");
-		//create a copy
-		ghost = Instantiate (baseEvent.selectedObject);
-	}
-
-	public void DragEventMethod(UnityEngine.EventSystems.BaseEventData baseEvent) {
-		Debug.Log(baseEvent.selectedObject.name + " triggered a drag event!");
-
-		//fade it
-		//ghost.GetComponent<Image> ().C = 0.5f;
-		//baseEvent.selectedObject is the GameObject that triggered the event,
-		// so we can access its components, destroy it, or do whatever.
-	}
 
 
 }
