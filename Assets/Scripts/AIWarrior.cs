95f5d71da6ce26cb671e2643d3d4943385e1656a
rogerdv <roger@ehtsc.tur.cu>
2016-04-14 13:10:52 -0500

    More stuff removed

--- Assets/Scripts/AIWarrior.cs
+++ Assets/Scripts/AIWarrior.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AIWarrior : BaseAI {
 
@@ -46,15 +47,15 @@ public class AIWarrior : BaseAI {
 		//Debug.Log("Warrior combat");
 		if (owner.equip[(int)ItemSlot.Weapon]==null) {
 			Debug.Log("I have no weapon equipped!");
-			foreach (var i in owner.inventory) {	//TODO: choose the best weapon
-				if (i.type=="weapon") {
-					owner.equip[(int)ItemSlot.Weapon] = i;
+			foreach (KeyValuePair<int,BaseItem> i in owner.inventory) {	//TODO: choose the best weapon
+				if (i.Value.type=="weapon") {
+					owner.equip[(int)ItemSlot.Weapon] = i.Value;
 					/* Test */
-					GameObject wprefab = Resources.Load ("Weapons/"+i.prefab) as GameObject;
+					GameObject wprefab = Resources.Load (i.Value.prefab) as GameObject;
 					GameObject weapon = Instantiate (wprefab) as GameObject; 
 					
 					foreach (Transform t in gameObject.GetComponentsInChildren<Transform>()) {
-						if (t.name == i.attach) { //"weapon_target_side.R_end"
+						if (t.name == i.Value.attach) { //"weapon_target_side.R_end"
 							weapon.transform.SetParent (t);
 							weapon.transform.localPosition = new Vector3(9.2f, -9.9f,-12.4f);
 							weapon.transform.localRotation = Quaternion.Euler (new Vector3(35.56f, 358.5f, 358.9f));
