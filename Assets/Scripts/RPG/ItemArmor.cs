86e3bb3e95ecab6c187ff8a566dd8734a96a3e83
rogerdv <roger@ehtsc.tur.cu>
2016-06-13 14:36:07 -0500

    Armor item class

---
+++ Assets/Scripts/RPG/ItemArmor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemArmor : BaseItem {
+
+	public override void Use (GameObject owner, GameObject target) {
+			 
+	}//Use
+	
+	public override void Equip(GameObject owner){
+		var OwnSc = owner.GetComponent<BaseCharacter> ();
+		if (swap) {
+
+		}
+		foreach (var m in mods) {
+			if (m.type == "strength") {
+				int currentMod = OwnSc.GetBaseAttribute((int)Attributes.Str).modifier;
+			}
+		}
+	}
+}
